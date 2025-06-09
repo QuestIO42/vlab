3bf522de773e2a7d4d9e95aa26b9a777c8cdf8e7
Enzo Murayama <enzomurayama@gmail.com>
2025-06-09 11:14:56 -0300

    fix: image path

--- html/editors/fpga/index.php
+++ html/editors/fpga/index.php
@@ -3,11 +3,9 @@
 <head>
 <title>Laborat&oacute;rio Remoto de L&oacute;gica Digital - DC/UFSCar</title>
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="/editors/fpga/styles.css">
 
-<style>
-body {background-color: #e0e0e0; color: #2b3856;}
-</style>
-        <script>
+<script>
             function changeSrc(loc) {
 			//document.getElementById('iframeId').save();
                 document.getElementById('iframeId').src = loc;
@@ -17,28 +15,29 @@ body {background-color: #e0e0e0; color: #2b3856;}
 	  var win = window.open(url, '_blank');
 	  win.focus();
 	}
-      </script>
-    </head>
-    <body>
-<center>
-<h1>Laborat&oacute;rio Remoto de L&oacute;gica Digital - DC/UFSCar</h1>
+</script>
+</head>
 
-</center>
-	<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
-	<button onclick="window.open('/camera.html', 'camera');">Camera</button>
-	<button onclick="window.location.href='/agenda'">Agenda</button>
-	<button onclick="changeSrc('synthesis.php?key=<?php echo $k;?>')">Sintetizar & Programar</button>
-	<button onclick="changeSrc('assembly.php?key=<?php echo $k;?>')">Assembly</button>
-	<button onclick="window.location.href='/about'">Sobre</button>
-    </body>
-<p> <b>Editor</b> (lembre-se do <b>[Save]</b> antes de prosseguir): 
-<br>
-<iframe name="editorFrame" id="editorFrame" src="editor.php" width="100%" height="337">
-</iframe>
-<hr>
-Console:
-<iframe id="iframeId" src="result.html" width="100%" height="300" style="border:none;">
-</iframe>
-</body>
+<body>
+	<center>
+	<h1>Laborat&oacute;rio Remoto de L&oacute;gica Digital - DC/UFSCar</h1>
+
+	</center>
+		<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
+		<button onclick="window.open('/camera.html', 'camera');">Camera</button>
+		<button onclick="window.location.href='/agenda'">Agenda</button>
+		<button onclick="changeSrc('synthesis.php?key=<?php echo $k;?>')">Sintetizar & Programar</button>
+		<button onclick="changeSrc('assembly.php?key=<?php echo $k;?>')">Assembly</button>
+		<button onclick="window.location.href='/about'">Sobre</button>
+		</body>
+	<p> <b>Editor</b> (lembre-se do <b>[Save]</b> antes de prosseguir): 
+	<br>
+	<iframe name="editorFrame" id="editorFrame" src="editor.php" width="100%" height="337">
+	</iframe>
+	<hr>
+	Console:
+	<iframe id="iframeId" src="result.html" width="100%" height="300" style="border:none;">
+	</iframe>
+	</body>
 </html>
 
