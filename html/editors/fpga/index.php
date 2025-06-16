6d66f2b6ba04583a67c8d2a7594e4c3f5d6a36b6
Enzo Murayama <enzomurayama@gmail.com>
2025-06-15 21:18:38 -0300

    chore: remove comments

--- html/editors/fpga/index.php
+++ html/editors/fpga/index.php
@@ -6,10 +6,10 @@
 <link rel="stylesheet" href="/editors/fpga/styles.css">
 
 <script>
-            function changeSrc(loc) {
-			//document.getElementById('iframeId').save();
-                document.getElementById('iframeId').src = loc;
-            }
+	function changeSrc(loc) {
+	//document.getElementById('iframeId').save();
+		document.getElementById('iframeId').src = loc;
+	}
 
 	function openInNewTab(url) {
 	  var win = window.open(url, '_blank');
@@ -24,78 +24,78 @@
 
 	<?php
 
-$dbHost=$_ENV["MYSQL_HOST"];
-$dbUnam=$_ENV["MYSQL_USER"]; 
-$dbPwrd=$_ENV["MYSQL_PASSWORD"];
-$dbName=$_ENV["MYSQL_DATABASE"];
+	$dbHost=$_ENV["MYSQL_HOST"];
+	$dbUnam=$_ENV["MYSQL_USER"]; 
+	$dbPwrd=$_ENV["MYSQL_PASSWORD"];
+	$dbName=$_ENV["MYSQL_DATABASE"];
 
-global $con;
-$con = mysqli_connect($dbHost, $dbUnam, $dbPwrd) or trigger_error("Erro ao acessar o Banco de Dados: " . mysqli_error($con));
+	global $con;
+	$con = mysqli_connect($dbHost, $dbUnam, $dbPwrd) or trigger_error("Erro ao acessar o Banco de Dados: " . mysqli_error($con));
 
-mysqli_select_db($con, $dbName) or trigger_error("Erro ao acessar o banco de dados: " . mysqli_error($con));
-
-
-if ($con) {
-        $query="set names utf8";
-        $result=mysqli_query($con,$query);
-}
+	mysqli_select_db($con, $dbName) or trigger_error("Erro ao acessar o banco de dados: " . mysqli_error($con));
 
 
-if (!empty($_GET['key']) && is_numeric($_GET['key'])) {
-
-	$k=$_GET['key'];
-	$sql = "SELECT * FROM slots WHERE catID=1 AND akey=\"$k\"";
-	$result = mysqli_query($con, $sql);
-
-	if (mysqli_num_rows($result) > 0) {
-		while($row = mysqli_fetch_assoc($result)) {
-
-			$s=$row["start"];
-			$e=$row["end"];
-
-			$dt = date('Y-m-d H:i:s');
-			echo "Sessão: Inicio: $s Fim: $e / Agora: $dt";
-
-			$A = strtotime($s); //gives value in Unix Timestamp (seconds since 1970)
-			$B = strtotime($e);
-			$C = strtotime($dt);
-
-			if ((($C < $A) && ($C > $B)) || (($C > $A) && ($C < $B)) ){
-			   echo " liberado";
-			} else {
-				echo "<br><br>Esta chave so permite acesso no horário entre $s e $e - <a href=/agenda>agende um novo horário</a><br>";
-?>
-	<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
-	<button onclick="window.open('/camera.php', 'camera');">Camera</button>
-	<button onclick="window.location.href='/agenda'">Agenda</button>
-	<button onclick="window.location.href='/about'">Sobre</button>
-<?php
-			   exit;
-			}
-		}
-	} else {
-	   echo "<BR><center>Chave não encontrada - <a href=/agenda>agende um horário</a></center>";
-?>   
-	<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
-	<button onclick="window.open('/camera.php', 'camera');">Camera</button>
-	<button onclick="window.location.href='/agenda'">Agenda</button>
-	<button onclick="window.location.href='/about'">Sobre</button>
-<?php
-	   exit;
+	if ($con) {
+			$query="set names utf8";
+			$result=mysqli_query($con,$query);
 	}
-	
-} else {
-	echo "<BR><center>Chave inválida - <a href=/agenda>agende um horário</a></center>";
-?>
-	<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
-	<button onclick="window.open('/camera.php', 'camera');">Camera</button>
-	<button onclick="window.location.href='/agenda'">Agenda</button>
-	<button onclick="window.location.href='/about'">Sobre</button>
-<?php
-	exit;
-}
 
-?>
+
+	if (!empty($_GET['key']) && is_numeric($_GET['key'])) {
+
+		$k=$_GET['key'];
+		$sql = "SELECT * FROM slots WHERE catID=1 AND akey=\"$k\"";
+		$result = mysqli_query($con, $sql);
+
+		if (mysqli_num_rows($result) > 0) {
+			while($row = mysqli_fetch_assoc($result)) {
+
+				$s=$row["start"];
+				$e=$row["end"];
+
+				$dt = date('Y-m-d H:i:s');
+				echo "Sessão: Inicio: $s Fim: $e / Agora: $dt";
+
+				$A = strtotime($s); //gives value in Unix Timestamp (seconds since 1970)
+				$B = strtotime($e);
+				$C = strtotime($dt);
+
+				if ((($C < $A) && ($C > $B)) || (($C > $A) && ($C < $B)) ){
+				echo " liberado";
+				} else {
+					echo "<br><br>Esta chave so permite acesso no horário entre $s e $e - <a href=/agenda>agende um novo horário</a><br>";
+	?>
+		<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
+		<button onclick="window.open('/camera.php', 'camera');">Camera</button>
+		<button onclick="window.location.href='/agenda'">Agenda</button>
+		<button onclick="window.location.href='/about'">Sobre</button>
+	<?php
+				exit;
+				}
+			}
+		} else {
+		echo "<BR><center>Chave não encontrada - <a href=/agenda>agende um horário</a></center>";
+	?>   
+		<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
+		<button onclick="window.open('/camera.php', 'camera');">Camera</button>
+		<button onclick="window.location.href='/agenda'">Agenda</button>
+		<button onclick="window.location.href='/about'">Sobre</button>
+	<?php
+		exit;
+		}
+		
+	} else {
+		echo "<BR><center>Chave inválida - <a href=/agenda>agende um horário</a></center>";
+	?>
+		<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
+		<button onclick="window.open('/camera.php', 'camera');">Camera</button>
+		<button onclick="window.location.href='/agenda'">Agenda</button>
+		<button onclick="window.location.href='/about'">Sobre</button>
+	<?php
+		exit;
+	}
+
+	?>
 
 	</center>
 		<button onclick="window.location.href='https://questio42.github.io/'">QuestI0</button>
